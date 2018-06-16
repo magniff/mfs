237237d48349bc4671788694cc93c2a488ee815b
Alexander Koshkin <akoshkin@ptsecurity.com>
2018-06-16 13:39:21 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="mfs",
+    description="Python mapping based filesystem.",
+    version="0.1",
+    license="GLWTPL",
+    platforms=["unix"],
+    author="magniff",
+    url="https://github.com/magniff/mfs",
+    packages=["mfs",],
+    zip_safe=False,
+)
+
